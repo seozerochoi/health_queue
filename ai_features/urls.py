a7e41d58582303406fa11168ec6bf25f85ba69f5
Hyeonjung Kim <khj78327@gmail.com>
2025-10-31 11:08:44 +0900

    BE, FE 디렉토리 파일 정리

--- ai_features/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import IdealTimeAPIView
-
-urlpatterns = [
-    path('ideal-time/<int:equipment_id>/', IdealTimeAPIView.as_view(), name='ideal-time'),
-]
--- gyms/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Equipment
-
-class EquipmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Equipment
-        # 앱에 보여줄 기구의 모든 정보를 포함합니다.
-        fields = '__all__'
