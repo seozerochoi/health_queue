aad62b632a7bf2cd4af53f77a2a94b258e14ce97
Hyeonjung Kim <khj78327@gmail.com>
2025-12-02 02:53:25 +0900

    feat: EquipmentDailyStats 모델을 admin에 등록하여 관리 기능 추가

--- BE/equipment/admin.py
+++ BE/equipment/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Equipment
-from .daily_stats_models import EquipmentDailyStats
 
 
 @admin.register(Equipment)
@@ -14,11 +13,3 @@ class EquipmentAdmin(admin.ModelAdmin):
 	search_fields = ('name', 'nfc_tag_id', 'arduino_id')
 
 
-@admin.register(EquipmentDailyStats)
-class EquipmentDailyStatsAdmin(admin.ModelAdmin):
-	list_display = (
-		'id', 'equipment', 'date', 'usage_count', 'total_usage_minutes', 'average_time_minutes'
-	)
-	list_filter = ('date', 'equipment__gym')
-	search_fields = ('equipment__name',)
-	readonly_fields = ('average_time_minutes', 'created_at', 'updated_at')
--- BE/reports/admin.py
+++ BE/reports/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Report, EquipmentDailyStats
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reporter', 'reported_user', 'equipment', 'report_type', 'status', 'created_at')
+    list_filter = ('report_type', 'status', 'created_at')
+    search_fields = ('reporter__username', 'reported_user__username', 'reason')
+    readonly_fields = ('created_at',)
+
+
+@admin.register(EquipmentDailyStats)
+class EquipmentDailyStatsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'equipment', 'date', 'usage_count', 'total_usage_minutes', 'average_time_minutes'
+    )
+    list_filter = ('date', 'equipment__gym')
+    search_fields = ('equipment__name',)
+    readonly_fields = ('average_time_minutes', 'created_at', 'updated_at')
+
