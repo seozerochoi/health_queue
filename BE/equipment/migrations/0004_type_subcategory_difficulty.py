b31da309dd65cc26a7414d7ccaac9c317af53c6a
jung-jaeil <jji001216@naver.com>
2025-12-04 19:52:19 +0900

    인바디 % 단위 적용

--- BE/equipment/migrations/0004_type_subcategory_difficulty.py
+++ BE/equipment/migrations/0004_type_subcategory_difficulty.py
@@ -1,4 +1,4 @@
-from django.db import migrations, models
+﻿from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ],
             ),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='equipment',
             name='subcategory',
             field=models.CharField(
