51f0811ff55b5f52a610e520d0efc7d4c543ba04
jung-jaeil <jji001216@naver.com>
2025-12-09 16:34:47 +0900

    대기시간 수정!!!!

--- FE/src/components/EquipmentList.tsx
+++ FE/src/components/EquipmentList.tsx
@@ -88,7 +88,7 @@ const EquipmentItemInner = ({ eq, onSelect, flashing }: EquipmentItemProps) => {
       case "in-use":
         return (
           <Badge className="bg-yellow-100 text-yellow-700">
-            사용 중 ({eq.timeRemaining}분 남음)
+            사용 중 ({eq.estimatedWaitTime ?? eq.timeRemaining}분 남음)
           </Badge>
         );
       case "waiting":
